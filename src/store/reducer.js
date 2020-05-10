180ac9d3db6dd25b999d8c9e712c017a6dd8e19c
zg1037 <1037595085@qq.com>
2020-05-10 17:49:09 +0800

    222

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,4 +1,5 @@
 import {combineReducers} from "redux-immutable";
+// redux-immutable 的作用是将 store中的js 对象转化为immutable对象
 // import {combineReducers} from "redux";
 import headerReducer from "../pages/header/store/reducer"
 
